662ba77586b468292f71c7fed195c35581181af3
Nathanael Dousa <84778099+NathanaelDousa@users.noreply.github.com>
2025-08-21 19:35:34 +0200

    Check

--- product-manager/app/Http/Controllers/ProductController.php
+++ product-manager/app/Http/Controllers/ProductController.php
@@ -1,8 +1,12 @@
 <?php
 
+
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use Illuminate\Http\Request; // <--- hier was je import voor Request
+use Symfony\Component\Process\Process; // <--- voor Process
+use Symfony\Component\Process\Exception\ProcessFailedException; // <--- voor ProcessFailedException
 
 class ProductController extends Controller
 {
@@ -15,11 +19,10 @@ class ProductController extends Controller
     public function askOllama(Request $request)
     {
         $productId = $request->productId;
-        $product = Product::find($productId);
+        $product = Product::findOrFail($productId); // vind product of gooi 404
 
         $prompt = "Geef een korte, vriendelijke omschrijving van het product: {$product->name}";
 
-        // Ollama aanroepen via command line
         $process = new Process(['ollama', 'run', 'llama2', $prompt]);
         $process->run();
 
@@ -31,5 +34,5 @@ class ProductController extends Controller
 
         return response()->json(['answer' => $answer]);
     }
-
 }
+
